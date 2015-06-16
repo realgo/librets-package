9af18c8ef5e1054a77bc33069c2ac4b372bbdc1c
Sean Reifschneider <sean@realgo.com>
2015-06-16 15:17:14 -0600

    Removing example files.

--- debian/librets.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the librets package
-#
-0 4	* * *	root	[ -x /usr/bin/librets_maintenance ] && /usr/bin/librets_maintenance
--- debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-liblibrets 1.6.1 librets (>> 1.6.1-0), librets (<< 1.6.1-99)
